d9636b6d52b33a53365fcefa4e50b14ba61df134
TrevorSteel12 <48173911+TrevorSteel12@users.noreply.github.com>
2020-03-03 18:30:30 +0000

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,22 @@
+document.write('\
+\
+<div class="w3-container w3-black"><h1><i>VAT Info</i></h1></div>\
+<div class="w3-bar w3-black">\
+  <a href="index.html" class="w3-bar-item w3-button w3-mobile">Home</a>\
+  <div class="w3-dropdown-hover">\
+    <button class="w3-button w3-mobile">Resources</button>\
+    <div class="w3-dropdown-content w3-bar-block w3-card-4">\
+      <a href="ResourcesHmrc.html" class="w3-bar-item w3-button">HMRC</a>\
+      <a href="#" class="w3-bar-item w3-button">Legislation</a>\
+      <a href="#" class="w3-bar-item w3-button">Case Law</a>\
+    </div>\
+  </div>\
+<div class="w3-dropdown-hover">\
+  <button class="w3-button w3-mobile">Sectors</button>\
+  <div class="w3-dropdown-content w3-bar-block w3-card-4">\
+    <a href="#" class="w3-bar-item w3-button">Local Authority</a>\
+    <a href="#" class="w3-bar-item w3-button">NHS/Central Gov</a>\
+  </div>\
+</div>\
+</div>\
+'); the
